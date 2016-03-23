a11e88d99c22277cb6cd5c7e51f70b4ee4148694
carlngan <carl@salonfrontdesk.com>
2016-03-22 17:43:51 -0700

    trying to fix dependency error

--- modules/claims/src/ClaimFactory.js
+++ modules/claims/src/ClaimFactory.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const moment = require("moment");
-var xsd = require('libxml-xsd');
+//var xsd = require('libxml-xsd');
 const fs = require('fs');
 const Claim = require("./Claim");
 const ClaimsSchema = require("../schemas/claims");
